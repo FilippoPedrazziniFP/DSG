dcc97532406143ea7ef33ca1a705d9ebfd668cdb
Luca Scannapieco <lucascannapieco.94@gmail.com>
2018-09-28 09:37:17 +0200

    Revert "Merge branch 'master' of https://github.com/FilippoPedrazziniFP/DSG"
    This reverts commit 5d3f08648a65902f18829594ee6f8f7cc31e7ef1, reversing
    changes made to 9d7c9c39509e0787d37a4c62dfc9208028fd7306.

---
+++ dsg/classifier/classifier.py
@@ -0,0 +1,237 @@
+from sklearn.metrics import roc_auc_score
+import numpy as np
+from itertools import islice
+import pickle
+import operator
+from itertools import chain
+from collections import defaultdict
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from dsg.data_loader import DataLoader
+from sklearn.preprocessing import StandardScaler
+from dsg.visualizer import Explorer
+
+class Classifier(object):
+	def __init__(self):
+		super(Classifier, self).__init__()
+
+	def create_cus_bond_freq_dictionary(self, df):
+		"""
+			The method creates the dictionary in which each
+			key is represented by (CustomerIdx, BondIdx) and 
+			the value is the number of historical occurrences 
+			betweem the two. 
+		"""
+		dictionary = df.groupby(['CustomerIdx', 'IsinIdx']).count()['TradeDateKey'].to_dict()
+		return dictionary
+
+	def create_bond_freq_dictionary(self, df):
+		"""
+			The method creates a dictionary with 
+			BondsIdx : frequency
+		"""
+		dictionary = df.groupby('IsinIdx').count()['TradeDateKey'].to_dict()
+		return dictionary
+
+	def create_cus_freq_dictionary(self, df):
+		"""		
+			The method creates a dictionary based on the frequence
+			of interaction during the considered period.
+			CustomerIdx : Frequency
+		"""
+		dictionary = df.groupby('CustomerIdx').count()['TradeDateKey'].to_dict()
+		return dictionary
+
+	def features_labels_split(self, X):
+		"""
+			The method splits the data into Features and Labels
+			returning a numpy array
+			@args
+				X : numpy array
+			@return
+				features : numpy array
+				labels : numpy array
+		"""
+		features = X[:,0:-1]
+		labels = X[:,-1]
+		return features, labels
+
+	def take(self, n, iterable):
+		return list(islice(iterable, n))
+
+	def create_bonds_features_dict(self, df_train):
+		"""
+			The method creates a dictionary with
+			BondIdx : features.
+		"""
+		bidx_freq = self.create_bond_freq_dictionary(df_train)
+		return bidx_freq
+
+	def create_customer_features_dict(self, df_train):
+		"""
+			The method creates a dictionary where the key 
+			represents the customer id and the value
+			is a list with 2 features 
+			[Normalized Frequency, 
+				Sum of Interest]
+		"""
+		# Creating Customer - Frequency Dictionary 
+		cidx_freq = self.create_cus_freq_dictionary(df_train)		
+		return cidx_freq
+
+	def create_cus_bond_features_dict(self, df_train):
+		"""
+			The method creates the dictionary with all the features related 
+			to the pair custonmer - bond.
+		"""
+		cbidx_freq = self.create_cus_bond_freq_dictionary(df_train)
+		return cbidx_freq
+
+	def fit(self, X_train_df, y_train_df):
+
+		# Create Customer Dictionary
+		self.customer_dictionary = self.create_customer_features_dict(X_train_df)
+
+		# Create Bond Dictionary
+		self.bond_dictionary = self.create_bonds_features_dict(X_train_df)
+
+		# Create Customer - Bond Dictionary
+		self.cus_bond_dictionary = self.create_cus_bond_features_dict(X_train_df)
+
+		# Create Train set with the dictionaries
+		train = self.create_set(y_train_df)
+
+		# Split Features and Labels
+		X, y = self.features_labels_split(train)
+
+		# Train Classifier
+		self.classifier = self.train_classifier(X, y)
+		return
+
+	def create_set(self, df):
+		"""
+			The method creates a matrix with 2 columns:
+			[CustomerIdx, NormalizedFrequency] given a dictionary
+			of CustomersIdx : Normalized Values.
+		"""
+		train = df.values
+		train_set = []
+		for sample in train:
+			row = []
+			
+			try:
+				customer_features = self.customer_dictionary[sample[0]]
+			except KeyError:
+				customer_features = 0
+			row.append(customer_features)
+			
+			try: 
+				bond_features = self.bond_dictionary[sample[1]]
+			except KeyError:
+				bond_features = 0
+			row.append(bond_features)
+
+			try: 
+				cus_bond_features = self.cus_bond_dictionary[(sample[0], sample[1])]
+			except KeyError:
+				cus_bond_features = 0
+			row.append(cus_bond_features)
+			
+			label = sample[2]
+			row.append(label)
+			train_set.append(row)
+		train_set = np.asarray(train_set)
+		return train_set
+
+	def train_classifier(self, X_train, y_train):
+		"""
+			Simple classifier to put a weight 
+			to the frequency feature.
+		"""
+		self.scaler = StandardScaler()
+		self.scaler.fit(X_train)
+		X_train = self.scaler.transform(X_train)
+
+		# Plot Distribution of Labels
+		# Explorer.plot_array(y_train)
+		print(y_train.mean())
+		print(y_train.std())
+		print(y_train.max())
+
+		# Clipping y_train
+		y_train = np.clip(y_train, 0, 5)
+
+		# Normalize Labels
+		y_train = y_train/y_train.max()
+
+		# From Continuos Values to Binary Classification
+		"""
+			From regression to classification.
+		"""
+
+		# Fit the model
+		model = LinearRegression()
+		model.fit(X_train, y_train)
+		return model
+
+	def get_max_value(self, dictionary):
+		max_value = 0
+		for k, v in dictionary.items():
+			if v > max_value:
+				max_value = v
+		return max_value
+
+	def print_dictionary(self, dictionary=None):
+		n_items = self.take(10, dictionary.items())
+		for key, val in n_items:
+			print(key, val)
+		return
+
+	def predict(self, X):
+		predictions = []
+		for sample in X:
+			features = []
+			
+			try: 
+				cust_features = self.customer_dictionary[sample[0]]
+			except KeyError:
+				cust_features = 0
+			
+			features.append(cust_features)
+
+			try:
+				bond_features = self.bond_dictionary[sample[1]]
+			except KeyError:
+				bond_features = 0
+
+			features.append(bond_features)
+
+			try:
+				cus_bond_features = self.cus_bond_dictionary[(sample[0], sample[1])]
+			except KeyError:
+				cus_bond_features = 0
+			
+			features.append(cus_bond_features)
+			
+			features = np.asarray(features)
+			features = np.reshape(features, (1, -1))
+			features = self.scaler.transform(features)
+			pred = self.classifier.predict(features)[0]
+			# pred = self.classifier.predict_proba(features)[0][0]
+			predictions.append(pred)
+		predictions = np.array(predictions)
+		print(predictions.max())
+		print(predictions[0:10])
+		predictions = np.clip(predictions, 0, 1)
+		return predictions
+
+	def evaluate(self, y_test_df):
+		X_test, y_test = self.features_labels_split_df(y_test_df)
+		y_pred = self.predict(X_test)
+		score = roc_auc_score(y_test, list(y_pred))
+		return score
+
+	def features_labels_split_df(self, test_df):
+		labels = test_df["CustomerInterest"]
+		features = test_df.drop(["CustomerInterest"], axis=1)
+		return features.values, labels.values
+		
---
+++ dsg/classifier/classifier_preprocessing.py
@@ -0,0 +1,70 @@
+import dsg.util as util
+from dsg.classifier.data_generator_class import DataGenerator
+
+class ClassifierPreprocessor(object):
+	def __init__(self, from_date, test_date, 
+			val_date, train_date):
+		super(ClassifierPreprocessor, self).__init__()
+		self.from_date = from_date
+		self.test_date = test_date
+		self.val_date = val_date
+		self.train_date = train_date
+
+	def fit(self, df):
+		return
+
+	def test_transform(self, df):
+		df = df.drop(["PredictionIdx", "DateKey", "BuySell"], axis=1)
+		X = df.values
+		return X
+
+	def transform(self, df):
+		return
+
+	def filter_data(self, df, date):
+		df = df[df["TradeDateKey"] >= date]
+		return df
+
+	def create_frequency_feature(self, df):
+		max_frequency = df['Frequency'].max()
+		df['Frequency'] = df.groupby('CustomerIdx')['CustomerIdx'].transform('count')
+		df['Probability'] = df['Frequency'].apply(lambda x: x/max_frequency)
+		return df
+
+	def fit_transform(self, df):
+		"""
+			The method drops the useless columns from
+			the DataFrame and splits the data into train 
+			test set based on the data
+
+			@args
+				df : DataFrame
+
+			@return
+				X_train, y_train, X_test, y_test : numpy array
+		"""
+		# Delete Holding Values
+		# df = df[df["TradeStatus"] != "Holding"]
+
+		# Drop Useless Columns
+		df = df.drop(["TradeStatus", "NotionalEUR", "Price"], axis=1)
+		df = df.sort_values("TradeDateKey", ascending=True)
+
+		# Filter Data 
+		df = self.filter_data(df, self.from_date)
+
+		# Train, test, val split
+		data_generator = DataGenerator()
+		X_train, y_train = data_generator.generate_train_set_regression(df, self.train_date, self.val_date)
+		y_test = data_generator.generate_test_set(df, self.test_date)
+		y_val = data_generator.generate_test_set(df, self.val_date, self.test_date)
+
+		# Entire Train
+		X, y = data_generator.generate_train_set_regression(df, self.test_date)
+
+		return X_train, y_train, y_test, y_val, X, y
+
+	def train_test_validation_split(self, features):
+		raise NotImplementedError
+
+		
