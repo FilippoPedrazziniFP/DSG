144acb124b3e2cc7e22885cb5ce4a62b3fd20127
Ubuntu <zione@zione.ivjbkuyog3jejbiqg3vltzfmwd.ax.internal.cloudapp.net>
2018-09-28 14:03:24 +0000

    methods to save/load pickle

--- dsg/loader.py
+++ dsg/loader.py
@@ -36,5 +36,11 @@ class DataLoader(object):
         with open(file_path, 'wb') as f:
             pickle.dump(file, f)
         return
+    
+    @staticmethod
+    def load_from_pickle(file_path):
+        with open(file_path, "rb") as f:
+            file = pickle.load(f)
+        return file
 
     
